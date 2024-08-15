99b2b2e870a98e65494db1be54f40e101cd71d23
Deetsy <amardeepdeepak2016@gmail.com>
2024-08-15 23:01:38 +0530

    successfully read games and genres

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,10 +4,17 @@ const getHome = async (req, res) => {
   res.render("home");
 };
 const getGenres = async (req, res) => {
-  res.render("genres");
+  const genres = await db.getAllGenres();
+  for (let genre of genres) {
+    genre.game = await db.getGameForGenre(genre.genreid);
+  }
+  res.render("genres", { genres: genres });
 };
 const getGames = async (req, res) => {
   const games = await db.getAllGames();
+  for (let game of games) {
+    game.genre = await db.getGenreForGame(game.gameid);
+  }
   res.render("games", { games: games });
 };
 module.exports = {
--- db/queries.js
+++ db/queries.js
@@ -4,11 +4,35 @@ async function getAllGames() {
   const { rows } = await pool.query("select * from games");
   return rows;
 }
+async function getGenreForGame(gameid) {
+  const { rows } = await pool.query(`select genre.genrename from genre
+join game_genre on genre.genreid=game_genre.genreid
+join games on game_genre.gameid=games.gameid
+where games.gameid=${gameid};`);
+  return rows;
+}
 //read genres
+async function getAllGenres() {
+  const { rows } = await pool.query("select * from genre");
+  return rows;
+}
+async function getGameForGenre(genreid) {
+  const { rows } = await pool.query(`select games.gamename from games
+    join game_genre on games.gameid=game_genre.gameid
+    join genre on game_genre.genreid=genre.genreid
+    where genre.genreid=${genreid};`);
+  return rows;
+}
+
 //create game
 //create genre
 //delete game
 //delete genre
 //update game
 //update genre
-module.exports = { getAllGames };
+module.exports = {
+  getAllGames,
+  getAllGenres,
+  getGenreForGame,
+  getGameForGenre,
+};
