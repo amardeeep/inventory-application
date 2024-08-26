29d89141c84e44eed8c2f801190d85a124762bb5
Deetsy <amardeepdeepak2016@gmail.com>
2024-08-26 19:42:59 +0530

    add body.css to all pages

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
-const path = require("node:path");
+const path = require("path");
 const { PORT } = require("./config");
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
+
 const homeRouter = require("./routes/homeRouter");
 
 app.use("/", homeRouter);
